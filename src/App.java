6913ef73c20874336268ee1490869d9a94fa9b4b
original <git@github.com:denis-poroshin/variables.git>
2024-03-13 18:26:40 +0300

    Домашняя работа выполнена (средний уровень).

--- src/App.java
+++ src/App.java
@@ -1,26 +1,28 @@
 public class App {
+    public static Book[] book = new Book[10];
     public static void main(String[] args) {
         Author author = new Author("Лев", "Толстой");
-        Book book = new Book(author,"Война и мир",  1867);
-        System.out.println(book);
-        book.setYear(1583);
-        System.out.println("Год публикации был изменен");
-        System.out.println(book);
-
-
         Author author1 = new Author("Александр", "Пушкин");
-        Book book1 = new Book(author1, "Евгений Онегин",  1833);
-        System.out.println(book1);
-        System.out.println("Год публикации был изменен");
-        book1.setYear(1600);
-        System.out.println(book1);
+        addBook(author, "Война и мир",  1867);
+        addBook(author1,  "Евгений Онегин",  1833);
+        printAllBook();
+    }
+    public static void addBook(Author author, String bookTitle, int year){
+        for (int i = 0; i < book.length; i++) {
+            if(book[i] == null){
+               Book newBook = new Book(author,bookTitle, year);
+               book[i] = newBook;
+               break;
+            }
+        }
+    }
+    public static void printAllBook(){
+        for (int i = 0; i < book.length; i++) {
+            if(book[i] == null){
+                break;
+            }
+            System.out.println(book[i].getAuthor() + ": " + book[i].getBookTitle() + ": " + book[i].getYear());
 
-
-
-
-
-
-
-
+        }
     }
 }
